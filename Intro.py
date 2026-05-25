e046cd4fc45c3ccdeb454e61b8d8db4b6c87aae9
Gigyanshu_kumar <gigyanshukumar2009@gmail.com>
2026-05-25 15:35:09 +0530

    Intro.py file
    This file contains a brief introduction about myself, my interest in cricket, and my target to learn Python basics in 1 month.

---
+++ Intro.py
@@ -0,0 +1,29 @@
+                                         # Intro.py
+
+my_name = "Gigyanshu kumar"                                   #my name
+age = 18                                                      #my current age
+home = "Sasaram,Bihar"                                        #my address
+intrest = "cricket"                                          #my intrests
+
+print("Hii there..")                                           #starting of intro
+
+print(f"1. I am {my_name} a {age} years old youth.")
+print()                                          #one line gap
+print(f"2. I am here to share some aspects of my personal life with you.")
+print()                                          #one line gap
+print(f"3. I belong to {home} a small town where there is no tech or coding environment.")
+print()                                          #one line gap
+print(f"4. I have been inclined towards {intrest} and outdoor activities since childhood and was a moderate student in my school.")
+print()                                          #one line gap
+print("5. I have also played cricket professionally on club level when i was in class 10th but i left it after 1 year because i was not mentally prepared for handle the pressure, whether it was physical pressure,mental pressure or financial pressure.")
+print()                                          #one line gap
+print("6. Now, I have completed my class 12th and i am going to be a college student.A lot of different kind of responsbilities are coming on my shoulders.")
+print()                                          #one line gap
+print("7. I chose Engineering as my career.")
+print("8. Honestly,when i heard about the word coding,I thought that it is very much difficult for me to learn but when I did my first code in my tablet, It gave me a lot of excitement and confidence as it was very easy for me to code.")
+print()                                          #one line gap
+print("9. Now,I am on my way to start and proceed my coding journey by learning from youtube tutorials and implement it on a coding platform pydroid 3.")
+print()                                          #one line gap
+print("10. I am targeting to learn complete basics of python language within 1 month and get switch on Object oriented programmes.")
+print()                                          #one line gap
+print("That's all from my side..Thank you....")

0bd527309e55c4c7224b3630f73058ef5b8cab98
Gigyanshu_kumar <gigyanshukumar2009@gmail.com>
2026-05-26 01:05:37 +0530

    #maths basic arithmatical operations.py
    Basic maths calculations like addition,subtraction, etc...

---
+++ #maths basic arithmatical operations.py
@@ -0,0 +1,48 @@
+                                 #maths arithmatical operations
+                                 
+
+a = 3                                      #first digit
+
+b = 7                                       #second digit
+
+print("a = 3")
+
+print()                                  #space
+
+print("b = 7")
+
+print()                                  #space
+
+sum = a + b                                  #sum of first and second digit
+
+difference = a - b                                  #difference of first and second digit
+
+product = a * b                                 #product of first and second digit
+
+divide = b / a                                     #division of second digit by first digit
+
+remain_a_b = a % b                       #remainder of b / a
+
+a_to_p_b= a ** b                                  #value of a^b
+
+print("The sum of ",a,"and",b,"is",sum)
+
+print()                                  #space
+
+print("The difference of",a,"and",b,"is",difference)
+
+print()                                  #space
+
+print("The product of ",a,"and",b,"is",product)
+
+print()                                  #space
+
+print("The Division of ",a,"and",b,"is",divide)
+
+print()                                  #space
+
+print("The remainder of ",a,"%",b,"is",remain_a_b)
+
+print()                                  #space
+
+print("The value of ",a,"^",b,"is",a_to_p_b)
